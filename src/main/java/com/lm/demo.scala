56bc696bddf405a305a19efd56b21059c033ba49
77954 <77954309@qq.com>
2019-06-02 16:56:24 +0800

    海明距离

--- src/main/java/com/lm/demo.scala
+++ src/main/java/com/lm/demo.scala
@@ -13,7 +13,6 @@ object demo {
     val content2="批处理：指事先将用户程序和数据装入卡带或磁带，并由计算机按照一定的顺序读取，使用户所执行这些程序和数据能够一并得到处理的方式。"
     val simhash=new Simhash
     //576671930010099734
-    //1045050699101040694
     val v1 = simhash.calSimhash(content2)
     println(v1)
   }
