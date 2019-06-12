e7d5aff275b7499072504cf7b9f483436c660f8d
77954 <77954309@qq.com>
2019-06-12 14:54:36 +0800

    分析

--- src/main/java/com/lm/analysis/HousePriceAnalyze.scala
+++ src/main/java/com/lm/analysis/HousePriceAnalyze.scala
@@ -1,12 +1,11 @@
 package com.lm.analysis
 
-import org.apache.spark.api.java.JavaRDD
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.{FloatType, StructField, StructType}
-import org.apache.spark.sql.{Row, SparkSession}
-import org.apache.spark.mllib.linalg.{Vector, Vectors}
-
-import scala.reflect.internal.util.TableDef.Column
+import org.apache.spark.ml.feature.StandardScaler
+import org.apache.spark.sql.types.{DoubleType, FloatType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.ml.linalg.Vector
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.regression.LinearRegression
 
 /**
   *
@@ -70,38 +69,83 @@ object HousePriceAnalyze {
         line(7).toFloat,
         line(8).toFloat)}).toJavaRDD()
 
-    var df = scc.createDataFrame(rdds, schema)
+    val df = scc.createDataFrame(rdds, schema)
 
     //预处理
     //1.处理房价
-    df=df.withColumn("medianHouseValue",df("medianHouseValue")/100000)
-
+    //3.去除无关值，比如经纬度
     /**
-      * 2
+      *
       * 每个家庭的平均房间数：roomsPerHousehold
       * 每个家庭的平均人数：populationPerHousehold
       * 卧室在总房间的占比：bedroomsPerRoom
       */
-    df = df.withColumn("roomsPerHousehold", df("totalRooms")/df("households"))
-        .withColumn("populationPerHousehold", df("population")/df("households"))
+    val df2=df.withColumn("medianHouseValue12",df("medianHouseValue")/100000).withColumn("roomsPerHousehold", df("totalRooms")/df("households"))
+      .withColumn("populationPerHousehold", df("population")/df("households"))
       .withColumn("bedroomsPerRoom", df("totalBedRooms")/df("totalRooms"))
 
-    //3.去除无关值，比如经纬度
-    df = df.select("medianHouseValue","totalBedrooms","population","households","medianIncome"
-      ,"roomsPerHousehold","populationPerHousehold","bedroomsPerRoom")
-    //Vectors.
+    /**
+      * 该区域房价的中位数,区域内总卧室数,区域内总人口数,区域内总家庭数,区域内人均收入中位数
+      * 每个家庭的平均房间数,每个家庭的平均人数,卧室在总房间的占比
+      */
+    import org.apache.spark.sql.functions._
+    val names="medianHouseValue12,totalBedrooms,population,households,medianIncome,roomsPerHousehold,populationPerHousehold,bedroomsPerRoom"
+   df2.select(names.split(",").map(name=>col(name).cast(DoubleType)):_*).createOrReplaceTempView("tmp2")
 
-    //拆分
-    val inputData=df.rdd.map(f=>{
-      val v=Vectors.dense(f.getDouble(1),f.getDouble(2),
-        f.getDouble(3),f.getDouble(4),f.getDouble(5),
-          f.getDouble(6),f.getDouble(7))
-       (f.getDouble(0),v)
+
+
+    val inputData=scc.sql("select * from tmp2").rdd.map(f=>{
+      val v:Vector=Vectors.dense(Array(f.getDouble(1),f.getDouble(2),f.getDouble(3),f.getDouble(4),f.getDouble(5),f.getDouble(6),f.getDouble(7)))
+      (f.getDouble(0),v)
     })
 
-    //val df2 = scc.createDataFrame(inputData,VectorMode.getClass)
-    //println(df2.take(2))
+    val df3: DataFrame = scc.createDataFrame(inputData).toDF("label", "features")
+
+    //setWithMean是否减均值。setWithStd是否将数据除以标准差。这里就是没有减均值但有除以标准差
+    //features训练模型，scaledFeatures 标准化
+    val scaler =new StandardScaler().setInputCol("features").setOutputCol("scaledFeatures").setWithMean(false).setWithStd(true)
+    //计算均值方差等参数
+    val scalerModel=scaler.fit(df3)
+    //标准化
+    val scalerModelDf=scalerModel.transform(df3)
+
+    //---------------------------------------------------------------预处理完成
+    //随机切割数据，weights权重，抽样的seed
+    val splits : Array[Dataset[Row]] = scalerModelDf.randomSplit(Array(0.8,0.2),123)
+     val trainData = splits(0)
+     val testData = splits(1)
+
+    // 建立模型，预测谋杀率Murder
+    // 设置线性回归参数
+    val lr1 = new LinearRegression()
+    val lr2 = lr1.setFeaturesCol("scaledFeatures").setLabelCol("label").setFitIntercept(true)
+    // RegParam：正则化
+    val lr3 = lr2.setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+    val lr = lr3
+    //训练集带入模型训练
+    val linearNodel = lr.fit(trainData)
+    val predicted = linearNodel.transform(testData)
+
+
+    //真实
+    //predicted.select("label").take(1).foreach(println(_))
+    //预测值
+    //predicted.select("prediction").take(1).foreach(println(_))
+
+    // 模型进行评价
+    val trainingSummary = linearNodel.summary
+    println(s"numIterations: ${trainingSummary.totalIterations}")
+    println(s"objectiveHistory: ${trainingSummary.objectiveHistory.toList}")
+    //残差
+    trainingSummary.residuals.show()
+    println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+    println(s"r2: ${trainingSummary.r2}")
+
+
+    println("输出预测结果")
+    val predict_result: DataFrame = predicted.selectExpr("scaledFeatures", "label", "round(prediction,1) as prediction")
+    predict_result.take(1).foreach(println(_))
+    sparkSession.stop()
   }
 
 }
-case class VectorMode(label:Double,features:Vector)
